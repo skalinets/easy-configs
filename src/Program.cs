33006c21e084b674b5d3cc28df2acb84a0f1bc45
Serhiy Kalinets <kalinets@gmail.com>
2021-10-22 17:36:24 +0300

    tries to use config with onchange

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,20 @@
+using Microsoft.Extensions.Options;
+using Src;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("my_config.json", optional: false, reloadOnChange: true);
+builder.Services.Configure<MySettings>(builder.Configuration.GetSection("Test"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => $"Hello World! The name is {app.Configuration["name"]}.");
+string GetHello()
+{
+    using var scope = app.Services.CreateScope();
+    var foo =
+        scope.ServiceProvider.GetService<IOptionsSnapshot<MySettings>>().Value.Name;
+    return $"Hello World! The name is {foo}.";
+}
+
+app.MapGet("/", GetHello);
 
 app.Run();
