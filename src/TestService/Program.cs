f480523aef5fe4d35d84143fc217e7dabc7585be
Serhiy Kalinets <kalinets@gmail.com>
2021-10-25 14:38:28 +0300

    add k8s, add uptime for service

--- src/TestService/Program.cs
+++ src/TestService/Program.cs
@@ -2,12 +2,14 @@ using Microsoft.AspNetCore;
 
 static string GetName(HttpContext ctx) => ctx.RequestServices.GetService<IConfiguration>()["name"];
 
+var startTime = DateTime.Now;
+
 var app = WebHost.CreateDefaultBuilder()
     .ConfigureAppConfiguration(x => x.AddJsonFile("my_config.json", optional: false, reloadOnChange: true))
     .Configure((ctx, builder) => builder
            .UseRouting()
            .UseEndpoints(
-               e => e.MapGet("/", c => c.Response.WriteAsync($"Hello World! The name is {GetName(c)}."))))
+               e => e.MapGet("/", c => c.Response.WriteAsync($"Hello World! The name is {GetName(c)}. Uptime is {DateTime.Now.Subtract(startTime)}"))))
     .Build();
 
 app.Run();
