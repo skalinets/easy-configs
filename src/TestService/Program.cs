d23e0f737ae735c24c1d54e1e13f42bddad729ad
Serhiy Kalinets <kalinets@gmail.com>
2021-10-27 11:41:11 +0300

    adds check for service start

--- src/TestService/Program.cs
+++ src/TestService/Program.cs
@@ -4,12 +4,16 @@ static string GetName(HttpContext ctx) => ctx.RequestServices.GetService<IConfig
 
 var startTime = DateTime.Now;
 
+const string ConfigPath ="/config/my_config.json";
+
+while (!File.Exists(ConfigPath)) { Thread.Sleep(1000); }
+
 var app = WebHost.CreateDefaultBuilder()
-    .ConfigureAppConfiguration(x => x.AddJsonFile("/config/my_config.json", optional: true, reloadOnChange: true))
+    .ConfigureAppConfiguration(x => x.AddJsonFile(ConfigPath, optional: true, reloadOnChange: true))
     .Configure((ctx, builder) => builder
            .UseRouting()
            .UseEndpoints(
-               e => e.MapGet("/", c => c.Response.WriteAsync($"Hello World! The name is {GetName(c)}. Uptime is {DateTime.Now.Subtract(startTime)}"))))
+               e => e.MapGet("/", c => c.Response.WriteAsync($"Hello World! The name is {GetName(c)}. Uptime is {DateTime.Now.Subtract(startTime)}. Some stats: {new {Environment.ProcessorCount, Memory = GC.GetTotalMemory(true)}}"))))
     .Build();
 
 app.Run();
